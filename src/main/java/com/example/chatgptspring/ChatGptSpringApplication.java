d0f97559579ee5d4e723868edf6cac4039e2038c
luxorious <vovan_194@mail.ru>
2023-05-19 18:17:37 +0200

    firstCommit

--- src/main/java/com/example/chatgptspring/ChatGptSpringApplication.java
+++ src/main/java/com/example/chatgptspring/ChatGptSpringApplication.java
@@ -1,13 +1,7 @@
 package com.example.chatgptspring;
 
-import com.example.chatgptspring.controller.ApplicationController;
-import com.example.chatgptspring.entity.Encryptor;
-import com.example.chatgptspring.entity.User;
-import com.example.chatgptspring.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class ChatGptSpringApplication {
