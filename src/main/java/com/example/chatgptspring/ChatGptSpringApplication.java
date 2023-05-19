bead958459039644e92eadbcb3b775a3a08f09b2
luxorious <vovan_194@mail.ru>
2023-05-19 18:17:13 +0200

    firstCommit

--- src/main/java/com/example/chatgptspring/ChatGptSpringApplication.java
+++ src/main/java/com/example/chatgptspring/ChatGptSpringApplication.java
@@ -13,17 +13,6 @@ import java.util.Arrays;
 public class ChatGptSpringApplication {
 
     public static void main(String[] args) {
-        Encryptor pw = new Encryptor();
-        String password = "password";
-        Integer[] arr = pw.encrypt(password);
-        System.out.println(Arrays.toString(arr));
-        String arr1 = pw.decryptPassword(arr);
-        System.out.println(arr1);
         SpringApplication.run(ChatGptSpringApplication.class, args);
-        User user = new User("name", "login", "email", "password");
-        System.out.println(user);
-
-
-
     }
 }
